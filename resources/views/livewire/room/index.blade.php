96936e8a1f4e88f39d0ac61714b9364dfb632b9d
F. Ahmed <19439535+hicka@users.noreply.github.com>
2025-07-18 19:29:00 +0500

    wip

--- resources/views/livewire/room/index.blade.php
+++ resources/views/livewire/room/index.blade.php
@@ -11,7 +11,7 @@
         <tr class="border-b text-left">
             <th class="py-2">Room Number</th>
             <th class="py-2">Type</th>
-            <th class="py-2">Status</th>
+            <th class="text-left py-2">Status</th>
             <th class="text-right py-2">Actions</th>
         </tr>
         </thead>
@@ -20,7 +20,32 @@
             <tr class="border-b">
                 <td class="py-2">{{ $room->room_number }}</td>
                 <td class="py-2">{{ $room->type?->name }}</td>
-                <td class="py-2 capitalize">{{ str_replace('_', ' ', $room->status) }}</td>
+                <td class="py-2">
+                    @switch($room->status)
+                        @case('available')
+                            <span class="inline-block px-2 py-1 text-xs font-semibold rounded-full bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300">
+                Available
+            </span>
+                            @break
+
+                        @case('occupied')
+                            <span class="inline-block px-2 py-1 text-xs font-semibold rounded-full bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300">
+                Occupied
+            </span>
+                            @break
+
+                        @case('maintenance')
+                            <span class="inline-block px-2 py-1 text-xs font-semibold rounded-full bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300">
+                Maintenance
+            </span>
+                            @break
+
+                        @default
+                            <span class="inline-block px-2 py-1 text-xs font-semibold rounded-full bg-gray-100 text-gray-800 dark:bg-slate-700 dark:text-gray-300">
+                Unknown
+            </span>
+                    @endswitch
+                </td>
                 <td class="py-2 text-right space-x-2">
                     <x-button size="sm" href="{{ route('rooms.edit', $room->id) }}">Edit</x-button>
                     <x-button size="sm" variant="danger" wire:click="delete({{ $room->id }})">Delete</x-button>
